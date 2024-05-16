94ec7cdcd7b97d95a8365f5e7f5a3bcf6c094c37
Ezio <e@e416.dev>
2024-05-15 18:25:59 -0600

    0.1.0

--- src/nadeo_api/__init__.py
+++ src/nadeo_api/__init__.py
@@ -1,10 +1,10 @@
 '''
 | Author:   Ezio416
 | Created:  2024-05-07
-| Modified: 2024-05-12
+| Modified: 2024-05-15
 
-- A library to assist with accessing Nadeo's undocumented (officially) web services API.
+- A library to assist with accessing Nadeo's web services API and the public Trackmania API (OAuth2).
 '''
 
-__version__: tuple = 0, 0, 2
+__version__: tuple = 0, 1, 0
 v:           tuple = __version__
