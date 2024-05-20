3d4bc699261d66b9742b402e8b80665b528a0fe4
Ezio <e@e416.dev>
2024-05-20 01:02:55 -0600

    simplify

--- scripts/flake.cmd
+++ scripts/flake.cmd
@@ -1,11 +1,5 @@
 @echo off
 REM run flake8 on nadeo_api modules
 REM E501 is the "line too long" error
-flake8 --extend-ignore=E501 ../src/nadeo_api/__init__.py
-flake8 --extend-ignore=E501 ../src/nadeo_api/auth.py
-flake8 --extend-ignore=E501 ../src/nadeo_api/core.py
-flake8 --extend-ignore=E501 ../src/nadeo_api/live.py
-flake8 --extend-ignore=E501 ../src/nadeo_api/meet.py
-flake8 --extend-ignore=E501 ../src/nadeo_api/oauth.py
-flake8 --extend-ignore=E501 ../src/nadeo_api/util.py
+flake8 --extend-ignore=E501 ../src/nadeo_api
 pause
