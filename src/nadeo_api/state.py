e8ebf472490a649aee423126d3aaabcdbcf6b36b
Ezio <e@e416.dev>
2025-08-05 00:31:09 -0600

    increase default wait time

--- src/nadeo_api/state.py
+++ src/nadeo_api/state.py
@@ -1,11 +1,11 @@
 '''
 | Author:   Ezio416
 | Created:  2025-08-04
-| Modified: 2025-08-04
+| Modified: 2025-08-05
 
 - Variables that be changed and used project-wide
 - You don't need to import this module - simply access these from the main module like `nadeo_api.<variable>`
 '''
 
 _last_request_timestamp:  int = 0
-wait_between_requests_ms: int = 500
+wait_between_requests_ms: int = 1000
