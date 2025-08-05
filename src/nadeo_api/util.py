b411395f5e0698b7d04f30977498bfdfdf59929c
Ezio <e@e416.dev>
2025-08-04 21:46:28 -0600

    util._log() + stamp()

--- src/nadeo_api/util.py
+++ src/nadeo_api/util.py
@@ -9,6 +9,9 @@
 
 import base64
 import re
+import sys
+import time
+import traceback as tb
 
 
 def account_id_from_login(account_login: str) -> str:
@@ -54,6 +57,34 @@ def account_login_from_id(account_id: str) -> str:
 
     return base64.urlsafe_b64encode(bytes.fromhex(account_id.replace('-', ''))).decode()[:-2]
 
+
+def _log(msg: str) -> None:
+    summary: tb.StackSummary = tb.extract_stack(sys._getframe())
+    print(
+        f'nadeo_api.{\
+        summary[-2].filename.replace('\\', '/').split('/')[-1].replace('.py', '')}.{\
+        summary[-2].name}: {msg}'
+    )
+
+
+def stamp(milliseconds: bool = False) -> int:
+    '''
+    - returns the current epoch time
+
+    Parameters
+    ----------
+    milliseconds: bool
+        - whether to return milliseconds instead of seconds
+        - default: False
+
+    Returns
+    -------
+    int
+        - the current epoch time
+    '''
+
+    now: float = time.time()
+    return int(now * (1000 if milliseconds else 1))
 
 def valid_uuid(uuid: str) -> bool:
     '''
