17ec2b864046886473b32e94ad93b71dc7cc7ab9
Ezio <e@e416.dev>
2026-04-19 16:58:54 -0600

    1.0.0

--- src/nadeo_api/__init__.py
+++ src/nadeo_api/__init__.py
@@ -1,9 +1,9 @@
 '''
-- A library to assist with accessing Nadeo's web services API and the public Trackmania API (OAuth2).
-- This is the main module - most of what you need is in sub-modules.
+- A library to assist with accessing Nadeo's web services API and the public Trackmania API (OAuth2)
+- This is the main module - most of what you need is in sub-modules
 '''
 
 from .util import *  # NOQA
 
 
-__version__: tuple = 0, 8, 0
+__version__: tuple = 1, 0, 0
